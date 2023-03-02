52f91239db2a7ea614aa64f1115ac67dd92a393b
gamiken <gamiken@kappa-lab.com>
2023-03-02 15:54:11 +0900

    CRUD

--- Makefile
+++ Makefile
@@ -3,3 +3,20 @@ run:
 
 check:
 	curl http://localhost:1323 	
+
+
+createUser:
+	curl -i -X POST 'http://localhost:1323/users' \
+	-H 'Content-Type: application/json' \
+	-d '{"name": "Dan","email": "dan@text.com"}'
+
+getUser:
+	curl -i http://localhost:1323/users/1
+
+updateUser:
+	curl -i -X PUT 'http://localhost:1323/users/1' \
+	-H 'Content-Type: application/json' \
+	-d '{"name": "ko","email": "ko@text.com"}'
+
+deleteUser:
+	curl -i -X DELETE http://localhost:1323/users/1		
--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -11,5 +13,52 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	e.POST("/users", createUser)
+	e.GET("/users/:id", getUser)
+	e.PUT("/users/:id", updateUser)
+	e.DELETE("/users/:id", deleteUser)
+
+	e.Use(middleware.Logger())
+
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+type User struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func createUser(c echo.Context) error {
+	u := &User{}
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	u.ID = uuid.NewString()
+	return c.JSON(http.StatusCreated, u)
+}
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	u := User{
+		ID:    id,
+		Name:  "Smith",
+		Email: "smith@test.com",
+	}
+	return c.JSON(http.StatusOK, u)
+}
+
+func updateUser(c echo.Context) error {
+	id := c.Param("id")
+	u := &User{}
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	u.ID = id
+	return c.JSON(http.StatusOK, u)
+}
+
+func deleteUser(c echo.Context) error {
+	return c.String(http.StatusNoContent, "ok")
 }
